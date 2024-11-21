a482f81731c3429db176c6bfbf3a051a7fc87150
codenameyizzz <legobatman201003@gmail.com>
2024-11-21 15:59:53 +0700

    test-auth-homepage

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Tracer Study Universitas XYZ</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+  <!-- Custom CSS -->
+  <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+  @yield('content')
+
+  <!-- Bootstrap JS Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('auth')->group(function () {
+    Route::get("/register", [AuthController::class, "getRegister"])->name("register");
+    Route::post("/register", [
+        AuthController::class,
+        "postRegister"
+    ])->name("post.register");
+    Route::get("/login", [AuthController::class, "getLogin"])->name("login");
+    Route::post("/login", [AuthController::class, "postLogin"])->name("post.login");
+    Route::get("/logout", [AuthController::class, "getLogout"])->name("logout");
+});
+Route::middleware('auth')->group(function () {
+    Route::get("/", [HomeController::class, "index"])->name("home");
 });
